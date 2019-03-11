0f81f487ef779e5f8828652bfe86d790b55a61c9
ShengHua Wu <fantasy0404@gmail.com>
2019-03-11 21:38:10 +0100

    [Server] Add new properties to Record type.

--- libra-server/Sources/App/Controllers/RecordsController.swift
+++ libra-server/Sources/App/Controllers/RecordsController.swift
@@ -3,3 +3,7 @@ import Vapor
 final class RecordsController: RouteCollection {
     func boot(router: Router) throws {}
 }
+
+private extension RecordsController {
+    // TODO: CRUD methods
+}
--- libra-server/Sources/App/Models/Record.swift
+++ libra-server/Sources/App/Models/Record.swift
@@ -4,9 +4,15 @@ import FluentPostgreSQL
 final class Record: Codable {
     var id: UUID?
     var title: String
+    var description: String?
+    var date: Date
     
-    init(title: String) {
+    // TODO: `category`, `creator`, `contributor`, `attachments` properties
+    
+    init(title: String, description: String?, date: Date) {
         self.title = title
+        self.description = description
+        self.date = date
     }
 }
 
