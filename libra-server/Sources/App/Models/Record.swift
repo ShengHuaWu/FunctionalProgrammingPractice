6eb6914b5f47ecd2b6a27b657e29812898f2aa38
ShengHuaWu <swu1@wayfair.com>
2019-03-19 16:57:42 +0100

    [Server] Append category to record.

--- libra-server/Sources/App/Models/Record.swift
+++ libra-server/Sources/App/Models/Record.swift
@@ -2,16 +2,23 @@ import Vapor
 import FluentPostgreSQL
 
 final class Record: Codable {
+    enum Category {
+        case expense(Double)
+        case other(String)
+    }
+    
     var id: UUID?
+    var category: Category
     var title: String
-    var description: String?
+    var note: String?
     var date: Date
     
-    // TODO: `category`, `creator`, `contributor`, `attachments` properties
+    // TODO: `creator`, `contributor`, `attachments` properties
     
-    init(title: String, description: String?, date: Date) {
+    init(category: Category, title: String, note: String?, date: Date) {
+        self.category = category
         self.title = title
-        self.description = description
+        self.note = note
         self.date = date
     }
 }
@@ -27,3 +34,32 @@ extension Record: Migration {}
 
 // MARK: - Parameter
 extension Record: Parameter {}
+
+// MARK: - Record Category Codable
+extension Record.Category: Codable {
+    enum CodingKeys: String, CodingKey {
+        case expense
+        case other
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        if let amount = try container.decodeIfPresent(Double.self, forKey: .expense) {
+            self = .expense(amount)
+        } else if let name = try container.decodeIfPresent(String.self, forKey: .other) {
+            self = .other(name)
+        } else {
+            self = .other(try container.decode(String.self, forKey: .other))
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .expense(let amount):
+            try container.encode(amount, forKey: .expense)
+        case .other(let name):
+            try container.encode(name, forKey: .other)
+        }
+    }
+}
