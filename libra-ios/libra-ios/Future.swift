eb2bad0c9e3994bf23a7dd54dfaffea107f94f69
ShengHuaWu <swu1@wayfair.com>
2019-04-12 17:11:29 +0200

    [iOS] Create future type.

---
+++ libra-ios/libra-ios/Future.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+struct Future<A> {
+    let run: (@escaping (A) -> Void) -> Void
+}
+
+extension Future {
+    func map<B>(_ f: @escaping (A) -> B) -> Future<B> {
+        return Future<B> { callback in
+            self.run { a in
+                let b = f(a)
+                callback(b)
+            }
+        }
+    }
+    
+    func flatMap<B>(_ f: @escaping (A) -> Future<B>) -> Future<B> {
+        return Future<B> { callback in
+            self.run { a in
+                let futureB = f(a)
+                futureB.run { b in
+                    callback(b)
+                }
+            }
+        }
+    }
+}
