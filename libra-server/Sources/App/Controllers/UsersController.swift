04b3f31f65c5942fbe456229c7c4117166b00fd4
ShengHuaWu <swu1@wayfair.com>
2019-06-26 10:01:13 -0400

    [Server] Update comments.

--- libra-server/Sources/App/Controllers/UsersController.swift
+++ libra-server/Sources/App/Controllers/UsersController.swift
@@ -4,7 +4,7 @@ import Crypto
 final class UsersController: RouteCollection {
     func boot(router: Router) throws {
         // Not protected
-        // TODO: API key?
+        // TODO: Authentication emails and SMS
         let usersGroup = router.grouped("users")
         usersGroup.post("signup", use: signupHandler)
         
