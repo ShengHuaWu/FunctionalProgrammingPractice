eba40001f330e34d187514a18c9bd8917f7f791a
ShengHuaWu <swu1@wayfair.com>
2019-07-30 15:54:44 +0200

    [Server] Refactor tests for signup.

--- libra-server/Tests/AppTests/UserTests.swift
+++ libra-server/Tests/AppTests/UserTests.swift
@@ -22,11 +22,11 @@ final class UserTests: XCTestCase {
         conn.close()
     }
     
-    func testThatSignUpUserSucceeds() throws {
+    func testThatSignupSucceeds() throws {
         let userInfo = AuthenticationBody.UserInfo(username: "sheng1", password: "12345678", firstName: "sheng", lastName: "wu", email: "sheng1@libra.co")
         let body = AuthenticationBody(userInfo: userInfo, osName: "mac os", timeZone: "CEST")
-        let signUpResponse = try app.sendRequest(to: "api/v1/users/signup", method: .POST, headers: ["Content-Type": "application/json"], body: body)
-        let receivedUser = try signUpResponse.content.decode(User.Public.self).wait()
+        let signupResponse = try app.sendRequest(to: "api/v1/users/signup", method: .POST, headers: ["Content-Type": "application/json"], body: body)
+        let receivedUser = try signupResponse.content.decode(User.Public.self).wait()
         
         XCTAssertNotNil(receivedUser.id)
         XCTAssertNotNil(receivedUser.token)
@@ -34,5 +34,12 @@ final class UserTests: XCTestCase {
         XCTAssertEqual(receivedUser.firstName, "sheng")
         XCTAssertEqual(receivedUser.lastName, "wu")
         XCTAssertEqual(receivedUser.email, "sheng1@libra.co")
+    }
+    
+    func testThatSignupThrowsBadRequestIfThereIsNoUserInfo() throws {
+        let body = AuthenticationBody(userInfo: nil, osName: "mac os", timeZone: "CEST")
+        let signupResponse = try app.sendRequest(to: "api/v1/users/signup", method: .POST, headers: ["Content-Type": "application/json"], body: body)
+        
+        XCTAssertEqual(signupResponse.http.status, .badRequest)
     }
 }
