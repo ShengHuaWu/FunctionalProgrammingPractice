b2c1d57dbf223f8fe013108aaec2e2754f10d11f
ShengHuaWu <swu1@wayfair.com>
2019-10-11 09:30:16 +0200

    [Server] Update comments.

--- libra-server/Package.swift
+++ libra-server/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
     .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
     .package(url: "https://github.com/vapor/auth.git", from: "2.0.1"),
-    .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "1.4.0") // ???: This fix the missing openssl issue
+    .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "1.4.0") // TODO: This fix the missing openssl issue ???
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor", "Authentication"]),
